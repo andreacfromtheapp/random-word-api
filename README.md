5182ddc4ad504fab448c796eed575bb26c80f85f
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-07-27 11:07:23 +0200

    feat: MVP

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Word API
+
+A simple API to use with my Speak and Spell toy project. To learn Axum and REST API development.
---
+++ src/router.rs
@@ -0,0 +1,34 @@
+use axum::{routing::get, routing::post, Router};
+use http::Method;
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
+
+use crate::handlers::{ping, word_create, word_delete, word_list, word_read, word_update};
+
+pub async fn create_router(dbpool: sqlx::Pool<sqlx::Sqlite>) -> axum::Router {
+    let origins = [
+        "http://localhost".parse().unwrap(),
+        "http://127.0.0.1".parse().unwrap(),
+    ];
+
+    Router::new()
+        .route("/alive", get(|| async { "ok" }))
+        .route("/ready", get(ping))
+        .nest(
+            "/v1",
+            Router::new()
+                .route("/words", get(word_list))
+                .route("/words/new", post(word_create))
+                .route(
+                    "/words/{id}",
+                    get(word_read).put(word_update).delete(word_delete),
+                ),
+        )
+        .with_state(dbpool)
+        .layer(
+            CorsLayer::new()
+                .allow_methods([Method::POST, Method::GET, Method::PUT, Method::DELETE])
+                .allow_origin(origins),
+        )
+        .layer(TraceLayer::new_for_http())
+}
