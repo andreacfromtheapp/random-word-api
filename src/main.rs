3bac4dad14ad08302b7c71e1be3454de8a256f16
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-07-29 22:31:42 +0200

    feat: split cli to mod. improved main with params
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,14 @@
 //!
 //! Its main purpose it to be a simple API to use with my Speak and Spell toy project. WIP. Come back soon.
 //!
-//! Run with
+//! Example run with custom port and SQLite db.file
 //!
 //! ```not_rust
-//! cargo run
+//! cargo run -p 5555 -d random-words.db
 //! ```
 
-use clap::Parser;
-use router::create_router;
-use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-use std::net::SocketAddr;
-use std::str::FromStr;
-use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
-
+/// Cli arguments and interface
+mod cli;
 /// Helpers for error handling
 mod error;
 /// API handlers
@@ -25,27 +20,10 @@ mod router;
 /// Model and business logic
 mod word;
 
-use word::Environment;
-
-/// Arguments for clap
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// The port number to bind the API to
-    #[arg(short, long, default_value_t = 3000)]
-    port: u16,
-
-    /// Environment: Development | Test | Production
-    #[arg(short, long, default_value_t = Environment::Development, value_enum)]
-    env: Environment,
-
-    /// The database connection URL
-    #[arg(short, long, default_value = "", env = "DATABASE_URL")]
-    database_url: String,
-}
-
 /// Configure tracing and logging
 fn init_tracing() {
+    use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+
     let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
         .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
 
@@ -59,13 +37,13 @@ fn init_tracing() {
         .init();
 }
 
-/// Configure the database Pool
+/// Configure the database pool
 async fn init_dbpool(db_url: &str) -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
-    let db_connection_str =
-        std::env::var(db_url).unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
+    use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+    use std::str::FromStr;
 
     let dbpool = SqlitePoolOptions::new()
-        .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true))
+        .connect_with(SqliteConnectOptions::from_str(db_url)?.create_if_missing(true))
         .await
         .expect("can't connect to database");
 
@@ -77,11 +55,14 @@ async fn init_dbpool(db_url: &str) -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Err
     Ok(dbpool)
 }
 
-/// Main. What else?!
+/// Tokio Main. What else?!
 #[tokio::main]
 async fn main() {
+    use clap::Parser;
+    use router::create_router;
+
     // Parse command-line args
-    let cli = Args::parse();
+    let cli = cli::Cli::parse();
 
     // Setup DB connection pool
     let dbpool = init_dbpool(&cli.database_url)
@@ -94,11 +75,8 @@ async fn main() {
     // Setup top-level router
     let router = create_router(dbpool).await;
 
-    // Setup socket binding IP address and port
-    let bind_addr = SocketAddr::from(([0, 0, 0, 0], cli.port));
-
     // Instantiate a listener on the socket address and port
-    let listener = tokio::net::TcpListener::bind(bind_addr)
+    let listener = tokio::net::TcpListener::bind((cli.address, cli.port))
         .await
         .expect("couldn't bind to address");
 
