42338f60af13307f918c2ccd4eef6f1bbc8a118f
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-07-29 15:38:10 +0200

    feat: added two more err to test
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- src/error.rs
+++ src/error.rs
@@ -11,10 +11,13 @@ pub enum Error {
     NotFound,
 }
 
+// These need revision when adding proper Anyhow and pattern
 impl From<sqlx::Error> for Error {
     fn from(err: sqlx::Error) -> Error {
         match err {
             sqlx::Error::RowNotFound => Error::NotFound,
+            sqlx::Error::Io(err) => Error::Sqlx(StatusCode::SERVICE_UNAVAILABLE, err.to_string()),
+            sqlx::Error::Protocol(str) => Error::Sqlx(StatusCode::SERVICE_UNAVAILABLE, str),
             _ => Error::Sqlx(StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
         }
     }
