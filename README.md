2a33ec69191f381c4388ed3fd6e83139f9d2ad4a
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-07-29 14:21:32 +0200

    docs: added rustdoc (and some minor fixes + clap)
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Random Word API
 
-A simple API to use with my Speak and Spell toy project.
+This is my first Rust API with Axum. The proverbial itch to learn API design and
+development with Rust.
+
+Its main purpose it to be a simple API to use with my
+[Speak and Spell](https://github.com/andreacfromtheapp/elm_speakandspell) toy
+project. WIP. Come back soon.
 
 ## Home
 
@@ -8,7 +13,5 @@ A simple API to use with my Speak and Spell toy project.
 
 ## Acknowledgments
 
-- Inspired by <https://github.com/mcnaveen/random-words-api>
-  - Not going to be as featureful.
-- Because it stopped working for
-  <https://github.com/andreacfromtheapp/elm_speakandspell>
+Inspired by <https://github.com/mcnaveen/random-words-api> (not going to be as
+featureful nor publicly usable)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
+//! This is my first Rust API with Axum. The proverbial itch to learn API design and development
+//! with Rust.
+//!
+//! Its main purpose it to be a simple API to use with my Speak and Spell toy project. WIP. Come back soon.
+
+use clap::Parser;
+use router::create_router;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+use std::net::SocketAddr;
+use std::str::FromStr;
+use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+
+/// Helpers for error handling
 mod error;
+/// API handlers
 mod handlers;
+/// Top-level router
 mod router;
+/// Model and business logic
 mod word;
 
-fn init_tracing() {
-    use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+use word::Environment;
 
+/// Arguments for clap
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The port number to bind the API to
+    #[arg(short, long, default_value_t = 3000)]
+    port: u16,
+
+    /// Environment: Development | Test | Production
+    #[arg(short, long, default_value_t = Environment::Development, value_enum)]
+    env: Environment,
+
+    /// The database connection URL
+    #[arg(short, long, default_value = "", env = "DATABASE_URL")]
+    database_url: String,
+}
+
+/// Configure tracing and logging
+fn init_tracing() {
     let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
         .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
 
@@ -19,13 +53,10 @@ fn init_tracing() {
         .init();
 }
 
-async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
-    use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-    use std::str::FromStr;
-    let _ = dotenvy::dotenv();
-
+/// Configure the database Pool
+async fn init_dbpool(db_url: &str) -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
     let db_connection_str =
-        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
+        std::env::var(db_url).unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
 
     let dbpool = SqlitePoolOptions::new()
         .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true))
@@ -40,21 +71,31 @@ async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
     Ok(dbpool)
 }
 
+/// Main. What else?!
 #[tokio::main]
 async fn main() {
-    use router::create_router;
+    // Parse command-line args
+    let cli = Args::parse();
 
+    // Setup DB connection pool
+    let dbpool = init_dbpool(&cli.database_url)
+        .await
+        .expect("couldn't initialize DB pool");
+
+    // Enable tracing using Tokio's https://tokio.rs/#tk-lib-tracing
     init_tracing();
 
-    let dbpool = init_dbpool().await.expect("couldn't initialize DB pool");
-
+    // Setup top-level router
     let router = create_router(dbpool).await;
 
-    let bind_addr = std::env::var("BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:3000".to_string());
+    // Setup socket binding IP address and port
+    let bind_addr = SocketAddr::from(([0, 0, 0, 0], cli.port));
 
+    // Instantiate a listener on the socket address and port
     let listener = tokio::net::TcpListener::bind(bind_addr)
         .await
         .expect("couldn't bind to address");
 
+    // Serve the word API
     axum::serve(listener, router).await.expect("server failed");
 }
