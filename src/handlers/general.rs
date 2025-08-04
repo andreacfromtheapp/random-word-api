41616f366b69f6901d50559f9992f26d266a5252
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-08-04 17:55:04 +0200

    feat: swaggerui and openapi with utoipa
    this is a good start. may improve later. I LOVE RUST!!!
    
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- src/handlers/general.rs
+++ src/handlers/general.rs
@@ -4,13 +4,23 @@ use sqlx::SqlitePool;
 
 use crate::error::AppError;
 
-/// Test the database connection from /ready
+/// Test the database connection from the API
+#[utoipa::path(
+    get,
+    path = "/ready",
+    operation_id = "api_db_connection_test",
+    tag = "generic_healthcheck_handlers",
+    responses(
+        (status = 200, description = "OK. The API can connect to the database", body = String),
+        (status = 500, description = "Internal server error. Couldn't connect to the database"),
+    )
+)]
 pub async fn ping(State(dbpool): State<SqlitePool>) -> Result<String, AppError> {
     use sqlx::Connection;
 
     let mut conn = dbpool.acquire().await?;
     conn.ping()
         .await
-        .map(|_| "ok".to_string())
+        .map(|_| "OK. The API can connect to the database".to_string())
         .map_err(Into::into)
 }
