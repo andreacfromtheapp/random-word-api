4b35a1d6113964c8ddc9a78181af9d9a6816b997
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-08-05 21:14:29 +0200

    fix: word unique constraint
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- migrations/20250727140350_create_words_table.up.sql
+++ migrations/20250727140350_create_words_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS words (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  word TEXT NOT NULL,
+  word TEXT NOT NULL UNIQUE,
   definition TEXT NOT NULL,
   pronunciation TEXT NOT NULL,
   created_at TEXT,
